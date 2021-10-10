0f83bd442cc250ed3780e5d55c2c84ad7b72044c
rhys puddephatt <rhys.puddephatt@btinternet.com>
2021-10-10 21:14:38 +0100

    add url

---
+++ Assets/getURlParam.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class getURlParam : MonoBehaviour
+{
+    [SerializeField]
+    UnityEvent<string> onUrl;
+    // Start is called before the first frame update
+    void Start()
+    {
+       onUrl.Invoke( Application.absoluteURL);
+    }
+
+}
